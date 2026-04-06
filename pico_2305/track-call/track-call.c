255deb308450eaefbdf46120dd3b422c5a7f3d38
thomasswindler <wigglebrows@gmail.com>
2026-04-06 08:02:02 -0600

    Worked on track call

--- pico_2305/track-call/track-call.c
+++ pico_2305/track-call/track-call.c
@@ -9,6 +9,7 @@
 // Maybe add in WS2812 insted of separate LEDs
 #define RELAY_PIN
 
+
 void TCinit()
 {
     // Initialize input pins
@@ -38,8 +39,27 @@ void TCinit()
     gpio_put(RELAY_PIN, 0);
 }
 
+void track_call()
+{
+    switch(state)
+    {
+        case 0:
+            put_pixel(pio, sm, urgb_u32(0, 0xff, 0)); // Green
+            break:
+        case 1:
+            put_pixel(pio, sm, urgb_u32(0, 0xff, 0xff)); // Yellow
+            break;
+        case 2:
+            put_pixel(pio, sm, urgb_u32(0xff, 0, 0)); // Red
+            break;
+        default: // When case doesn't equal 0, 1, 2
+            break;
+    }
+}
 
-int main()
+
+
+int switch_test_main()
 {
     int state = 0;
     stdio_init_all();
@@ -54,6 +74,7 @@ int main()
             gpio_put(GREEN_PIN, 0);
 
             state++;
+            sleep_ms(1000);
             break;
 
         case 1:
@@ -62,6 +83,7 @@ int main()
             gpio_put(GREEN_PIN, 0);
 
             state++;
+            sleep_ms(1000);
             break;
 
         case 2:
@@ -70,6 +92,7 @@ int main()
             gpio_put(GREEN_PIN, 1);
 
             state++;
+            sleep_ms(1000);
             break;
 
         default:
@@ -78,12 +101,122 @@ int main()
             gpio_put(GREEN_PIN, 0);
 
             state = 0;
+            sleep_ms(1000);
             break;
         }
 
-        
-
         printf("Hello, world!\n");
-        sleep_ms(1000);
     }
+}
+
+void main()
+{
+    stdio_init_all();
+    TCinit();
+
+    // Initialize WS2812 LEDs
+    PIO pio;
+    uint sm;
+    uint offset;
+    
+    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&ws2812_program, &pio, &sm, &offset, WS2812_PIN, 1, true);
+    hard_assert(success);
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+    //
+    uint state = 0;
+    while (true) {
+        
+    }    
+
+
+}
+
+
+// ------------------------------------------------------------------- //
+
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Highly reduced and modified for Mechatrnonics lab by D. W. Frame 2025
+ */
+
+inline void put_pixel(PIO pio, uint sm, uint32_t pixel_grb) {
+    pio_sm_put_blocking(pio, sm, pixel_grb << 8u);
+}
+
+inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
+    return
+            ((uint32_t) (r) << 8) |
+            ((uint32_t) (g) << 16) |
+            (uint32_t) (b);
+}
+
+inline uint32_t urgbw_u32(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+    return
+            ((uint32_t) (r) << 8) |
+            ((uint32_t) (g) << 16) |
+            ((uint32_t) (w) << 24) |
+            (uint32_t) (b);
+}
+
+int ws2812_main() { //ws2812_main() --- IGNORE ---
+    //set_sys_clock_48();
+    stdio_init_all();
+    printf("WS2812 Smoke Test, using pin %d\n", WS2812_PIN);
+
+    // todo get free sm
+    PIO pio;
+    uint sm;
+    uint offset;
+
+    // This will find a free pio and state machine for our program and load it for us
+    // We use pio_claim_free_sm_and_add_program_for_gpio_range (for_gpio_range variant)
+    // so we will get a PIO instance suitable for addressing gpios >= 32 if needed and supported by the hardware
+    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&ws2812_program, &pio, &sm, &offset, WS2812_PIN, 1, true);
+    hard_assert(success);
+
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
+
+    int t = 0;
+    while (true) 
+    {
+        put_pixel(pio, sm, urgb_u32(0xff, 0, 0));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0, 0xff, 0));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0, 0, 0xff));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0, 0, 0));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0xff, 0xff, 0xff));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0, 0, 0));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0xff, 0xff, 0xff));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0, 0, 0));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0xff, 0xff, 0xff));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0, 0, 0));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0xff, 0xff, 0xff));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0, 0, 0));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0xff, 0xff, 0xff));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0, 0, 0));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0xff, 0xff, 0xff));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0, 0, 0));
+        sleep_ms(1000);
+        put_pixel(pio, sm, urgb_u32(0xff, 0xff, 0xff));
+        sleep_ms(1000);    
+    }
+
+    // This will free resources and unload our program
+    pio_remove_program_and_unclaim_sm(&ws2812_program, pio, sm, offset);
 }
