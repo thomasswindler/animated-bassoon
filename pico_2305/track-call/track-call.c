c5f6da3ae2b41e4075afa2205b0b4b9cae3c29f1
thomasswindler <wigglebrows@gmail.com>
2026-04-06 20:15:21 -0600

    Added 4 input buttons.

--- pico_2305/track-call/track-call.c
+++ pico_2305/track-call/track-call.c
@@ -4,35 +4,62 @@
 #include "hardware/timer.h"
 #include "MECHws2812.h"
 
-#define TC_PIN 16
-#define RESET_PIN 17
+#define TC1_PIN 16
+#define TC2_PIN 17
+#define TC3_PIN 18
+#define TC4_PIN 19
+#define RESET_PIN 20
 // #define RED_PIN
 // #define YELLOW_PIN
 // #define GREEN_PIN
 #define RELAY_PIN 15
 
-volatile bool tc_pressed = false;
+volatile bool tc1_pressed = false;
+volatile bool tc2_pressed = false;
+volatile bool tc3_pressed = false;
+volatile bool tc4_pressed = false;
 volatile bool reset_pressed = false;
-struct repeating_timer tc_timer;
+struct repeating_timer tc1_timer;
+struct repeating_timer tc2_timer;
+struct repeating_timer tc3_timer;
+struct repeating_timer tc4_timer;
 struct repeating_timer reset_timer;
 
 void gpio_callback(uint gpio, uint32_t events);
-bool tc_debounce_callback(struct repeating_timer *t);
+bool tc1_debounce_callback(struct repeating_timer *t);
+bool tc2_debounce_callback(struct repeating_timer *t);
+bool tc3_debounce_callback(struct repeating_timer *t);
+bool tc4_debounce_callback(struct repeating_timer *t);
 bool reset_debounce_callback(struct repeating_timer *t);
 
 void TCinit()
 {
     // Initialize input pins
-    gpio_init(TC_PIN);
-    gpio_set_dir(TC_PIN, GPIO_IN);
-    gpio_pull_up(TC_PIN);
+    gpio_init(TC1_PIN);
+    gpio_set_dir(TC1_PIN, GPIO_IN);
+    gpio_pull_up(TC1_PIN);
+
+    gpio_init(TC2_PIN);
+    gpio_set_dir(TC2_PIN, GPIO_IN);
+    gpio_pull_up(TC2_PIN);
+
+    gpio_init(TC3_PIN);
+    gpio_set_dir(TC3_PIN, GPIO_IN);
+    gpio_pull_up(TC3_PIN);
+
+    gpio_init(TC4_PIN);
+    gpio_set_dir(TC4_PIN, GPIO_IN);
+    gpio_pull_up(TC4_PIN);
 
     gpio_init(RESET_PIN);
     gpio_set_dir(RESET_PIN, GPIO_IN);
     gpio_pull_up(RESET_PIN);
 
     // Set up interrupts for button debounce
-    gpio_set_irq_enabled_with_callback(TC_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(TC1_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(TC2_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(TC3_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(TC4_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
     gpio_set_irq_enabled_with_callback(RESET_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
     // Initialize output pins
@@ -55,16 +82,43 @@ void TCinit()
 }
 
 void gpio_callback(uint gpio, uint32_t events) {
-    if (gpio == TC_PIN) {
-        add_repeating_timer_ms(50, tc_debounce_callback, NULL, &tc_timer);
+    if (gpio == TC1_PIN) {
+        add_repeating_timer_ms(50, tc1_debounce_callback, NULL, &tc1_timer);
+    } else if (gpio == TC2_PIN) {
+        add_repeating_timer_ms(50, tc2_debounce_callback, NULL, &tc2_timer);
+    } else if (gpio == TC3_PIN) {
+        add_repeating_timer_ms(50, tc3_debounce_callback, NULL, &tc3_timer);
+    } else if (gpio == TC4_PIN) {
+        add_repeating_timer_ms(50, tc4_debounce_callback, NULL, &tc4_timer);
     } else if (gpio == RESET_PIN) {
         add_repeating_timer_ms(50, reset_debounce_callback, NULL, &reset_timer);
     }
 }
 
-bool tc_debounce_callback(struct repeating_timer *t) {
-    if (!gpio_get(TC_PIN)) { // still low, pressed
-        tc_pressed = true;
+bool tc1_debounce_callback(struct repeating_timer *t) {
+    if (!gpio_get(TC1_PIN)) { // still low, pressed
+        tc1_pressed = true;
+    }
+    return false; // don't repeat
+}
+
+bool tc2_debounce_callback(struct repeating_timer *t) {
+    if (!gpio_get(TC2_PIN)) { // still low, pressed
+        tc2_pressed = true;
+    }
+    return false; // don't repeat
+}
+
+bool tc3_debounce_callback(struct repeating_timer *t) {
+    if (!gpio_get(TC3_PIN)) { // still low, pressed
+        tc3_pressed = true;
+    }
+    return false; // don't repeat
+}
+
+bool tc4_debounce_callback(struct repeating_timer *t) {
+    if (!gpio_get(TC4_PIN)) { // still low, pressed
+        tc4_pressed = true;
     }
     return false; // don't repeat
 }
@@ -172,9 +226,9 @@ void main()
     uint state = 0;
     track_call(pio, sm, state); // Initialize to state 0
     while (true) {
-        if (tc_pressed) { // Only allow TC press if relay is off
-            tc_pressed = false;
-            // Handle TC button press, e.g., change state
+        if (tc1_pressed) { // TC1 button pressed
+            tc1_pressed = false;
+            // Handle TC1 button press
             if (state < 3) {
                 track_call(pio, sm, state);
                 gpio_put(RELAY_PIN, 1);
@@ -186,6 +240,21 @@ void main()
             else if (state >= 3) {
                 track_call(pio, sm, state); // Flash white
             }
+        }
+
+        if (tc2_pressed) { // TC2 button pressed
+            tc2_pressed = false;
+            // Handle TC2 button press
+        }
+
+        if (tc3_pressed) { // TC3 button pressed
+            tc3_pressed = false;
+            // Handle TC3 button press
+        }
+
+        if (tc4_pressed) { // TC4 button pressed
+            tc4_pressed = false;
+            // Handle TC4 button press
         }
 
         if (reset_pressed) {
